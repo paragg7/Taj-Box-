7b2e55ffced92683386477be13120a9b8fea25da
Parag Sagar <2018parag@gmail.com>
2026-02-21 15:03:11 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANwkn-k-9WmwYBWGFQCcYbN3QMAp3fI_Q",
-  authDomain: "tajboxxx.firebaseapp.com",
-  projectId: "tajboxxx",
-  storageBucket: "tajboxxx.firebasestorage.app",
-  messagingSenderId: "786715951257",
-  appId: "1:786715951257:web:cf5ad8fada605f70bb7808",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
